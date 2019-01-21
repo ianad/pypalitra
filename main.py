18cc2913a43bdc6fb6e190eabb3d38b7be255d9d
ianad <ian.dalton@ucf.edu>
2019-01-21 15:10:38 -0500

    utf-8 encode

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def main():
     """
     img_paths = [path.join('sourceimg',img) for img in listdir(path.join('sourceimg'))]
     for img in img_paths:
-        print(img,'\n')
-        print(image_to_string(Image.open(img)))
+        print(img)
+        print(image_to_string(Image.open(img).encode('utf-8')))
 
 
 if __name__ == "__main__":
