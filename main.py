d602fb6bfd1355ab3b59b4c1607555ea908c16de
ianad <ian.dalton@ucf.edu>
2019-01-21 17:15:31 -0500

    ignore ascii encode errors

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main():
     img_paths = [(source / img) for img in listdir(source)]
     for img in img_paths:
         print(img)
-        print(image_to_string(Image.open(img)))
+        # print(type(image_to_string(Image.open(img))))
+        print(image_to_string(Image.open(img)).encode('ascii',errors='ignore'))
 
 
 if __name__ == "__main__":
